644421db45ed4d9ae3127f21c6ab55f96fef3a51
Mehrdad <m.mirsamie@gmail.com>
2019-12-22 13:18:39 +0330

    .

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -1,5 +1,7 @@
 <!-- =============================================== -->
-
+@php
+$isCustomer = isset($isCustomer)?$isCustomer:false;
+@endphp
 <!-- Left side column. contains the sidebar -->
 <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
