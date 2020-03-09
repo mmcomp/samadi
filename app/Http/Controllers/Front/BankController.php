216bc4e3093dc33d368e6d0ca9a22f2b23585006
mirsamie <mirsamie@PROGRAMMER11>
2020-03-09 09:05:05 +0330

    .

--- app/Http/Controllers/Front/BankController.php
+++ app/Http/Controllers/Front/BankController.php
@@ -86,7 +86,7 @@ class BankController extends Controller
         ]);
         dump($order);
         // xS5zueZZNfD4tB
-        $yekResult = $this->yekPay($order->total, $order->reference, $order->customer_id)
+        $yekResult = $this->yekPay($order->total, $order->reference, $order->customer_id);
         dd($yekResult);
     }
 
