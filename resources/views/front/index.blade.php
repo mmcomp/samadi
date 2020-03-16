e554a66a02db675e07c3b6a491e40b4f7e3398f3
mirsamie <mirsamie@PROGRAMMER11>
2020-03-16 11:45:25 +0330

    .

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -728,6 +728,18 @@ a.page-link {
         placeholder: "{{__('app.search_filter')}}",
         allowClear: true
     });
+    var search = "{{ $search }}";
+    if(search!='') {
+        $(document).ready(function() {
+            $("a.page-link").each(function(id, feild) {
+                let href = $(feild).prop('href');
+                if(href.indexOf('search')<0) {
+                    href += `&search=${search}`
+                }
+                $(feild).prop('href', href);
+            });
+        });
+    }
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
